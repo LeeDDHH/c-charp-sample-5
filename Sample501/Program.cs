3391b199932388df34c0636b0048963f6081ec5b
DongHeon Lee <niche3600@gmail.com>
2022-03-27 11:35:41 +0900

    static class example

--- Sample501/Program.cs
+++ Sample501/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Sample501
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data[] d = new Data[2];
+            Data.ShowNumber();
+
+            for (int i = 0; i < d.Length; i++)
+            {
+                d[i] = new Data(i * 100);
+                Data.ShowNumber();
+            }
+        }
+    }
+}
