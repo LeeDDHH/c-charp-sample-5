a8aba9304624ac9c650f462ae33ffa229dd2684e
DongHeon Lee <niche3600@gmail.com>
2022-03-27 17:27:49 +0900

    use abstract class to make array example

--- Sample510/Program.cs
+++ Sample510/Program.cs
@@ -4,12 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Crow c = new Crow();
-            Sparrow s = new Sparrow();
-            Console.Write($"{c.Name}: ");
-            c.Sing();
-            Console.Write($"{s.Name}: ");
-            s.Sing();
+            // Crow c = new Crow();
+            // Sparrow s = new Sparrow();
+            // Console.Write($"{c.Name}: ");
+            // c.Sing();
+            // Console.Write($"{s.Name}: ");
+            // s.Sing();
+
+            // Birdクラスの変数の配列を生成
+            Bird[] b = new Bird[2];
+
+            // b[0]に、Crowクラスのインスタンスを生成
+            b[0] = new Crow();
+            // b[1]に、Sparrowクラスのインスタンスを生成
+            b[1] = new Sparrow();
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                Console.Write($"{b[i].Name} : ");
+                b[i].Sing();
+            }
         }
     }
 }
