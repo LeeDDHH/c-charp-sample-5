d4601d79f637f33059adfafc1dc8d7d2aef68e36
DongHeon Lee <niche3600@gmail.com>
2022-03-27 13:20:35 +0900

    trigonometric function example

---
+++ Sample504/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Sample504
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"角度の入力：");
+            double angle = double.Parse(Console.ReadLine());
+            double rad = Math.PI * angle / 180.0;
+            Console.WriteLine($"sin{angle}= {Math.Sin(rad)}");
+            Console.WriteLine($"cos{angle}= {Math.Cos(rad)}");
+            Console.WriteLine($"tan{angle}= {Math.Tan(rad)}");
+        }
+    }
+}
