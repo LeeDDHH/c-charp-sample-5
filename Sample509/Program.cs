4f4a0bf6dca53c51f024bf4f6831d0575b144bf1
DongHeon Lee <niche3600@gmail.com>
2022-03-27 16:32:53 +0900

    object class example

---
+++ Sample509/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Sample509
+{
+    class Program
+    {
+        public void Foo()
+        {
+            Console.WriteLine($"Foo");
+        }
+        static void Main(string[] args)
+        {
+            Program s1 = new Program();
+            Program s2 = s1;
+            Console.WriteLine($"{s1.ToString()}");
+            Console.WriteLine($"{s1.Equals(s2)}");
+            Console.WriteLine($"{s1.GetType()}");
+            Console.WriteLine($"{s1.GetHashCode()}");
+            Console.WriteLine($"{s1.MemberwiseClone()}");
+        }
+    }
+}
