deefd40f21b268585f44ecedf98a24ed6b3ba6bf
DongHeon Lee <niche3600@gmail.com>
2022-03-27 12:20:28 +0900

    static method instance method example

---
+++ Sample502/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Sample502
+{
+    class Program
+    {
+        private static int snum = 100;
+
+        public int inum = 200;
+
+        public static void Foo()
+        {
+            Console.WriteLine("fooメソッド（staticメソッド）");
+        }
+
+        public void Bar()
+        {
+            Console.WriteLine("fooメソッド（インスタンスメソッド）");
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine($"pのインスタンスフィールド: inum ={p.inum}");
+            Console.WriteLine($"Programのクラスフィールド: snum ={snum}");
+            Foo();
+            p.Bar();
+        }
+    }
+}
